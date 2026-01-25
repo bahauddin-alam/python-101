726448d26f4c90ce6229e8cc0e0ce96c9ace7523
Bahauddin Alam <bahauddinalamx@gmail.com>
2026-01-26 03:18:15 +0530

    ch-7 completed

---
+++ 02_datatypes/chapter_7.py
@@ -0,0 +1,17 @@
+# LECTURE -- 16. Tuples and Membership Testing
+
+masala_spices = ("cardamon", "cloves", "cinnamon")
+
+(spice1, spice2, spice3) = masala_spices
+
+print(f"Main masala spices: {spice1}, {spice2}, {spice3}")
+
+ginger_ratio, cardamon_ratio = 2, 1
+
+print(f"Ratio is G :{ginger_ratio} and C: {cardamon_ratio}")
+ginger_ratio, cardamon_ratio = cardamon_ratio, ginger_ratio
+print(f"Ratio is G :{ginger_ratio} and C: {cardamon_ratio}")
+
+# membership testing it is checked by "in" checks the box, the, the purse, the container about belonging of specific material
+print(f"Is ginger in masala spices? {'ginger' in masala_spices}")
+print(f"Is cloves in masala spices? {'cloves' in masala_spices}")
