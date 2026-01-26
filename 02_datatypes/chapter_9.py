f6bc89ef2bd5bff0fa33c6aaa83f5625cf92996a
Bahauddin Alam <bahauddinalamx@gmail.com>
2026-01-26 20:07:56 +0530

    Ch-9(sets) completed

---
+++ 02_datatypes/chapter_9.py
@@ -0,0 +1,13 @@
+essential_spices = {"cardamon", "ginger", "cinnamon"}
+optional_spices = {"cloves", "ginger", "black pepper"}
+
+all_spices = essential_spices | optional_spices # This is called union of sets by |
+print(f"All spices {all_spices}")
+
+common_spices = essential_spices & optional_spices # This is called intersection of sets by &
+print(f"Common spices {common_spices}")
+
+only_in_essential = essential_spices - optional_spices # This is called unqiue items in a set -
+print(f"Only in essential spices {only_in_essential}")
+
+print(f"Is cloves in optional spices: {'cloves' in optional_spices}")
