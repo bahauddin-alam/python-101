1c6f92e6df9786ffc1bf5276da3da5f928f08378
Bahauddin Alam <bahauddinalamx@gmail.com>
2026-01-26 18:53:42 +0530

    completed lec-18 Operator overloading and bytearray in python within the codebase of lec-17

--- 02_datatypes/chapter_8.py
+++ 02_datatypes/chapter_8.py
@@ -1,4 +1,4 @@
-#LECTURE -- 17. Basics of List in Python
+#LECTURE -- 17. Basics of List in Python + 18. Operator overloading and bytearray in python
 
 ingredients = ["water", "milk", "black tea"]
 ingredients.append("sugar") # Adding method
@@ -34,3 +34,14 @@ Strings vs. Strings: Works (Alphabetical)."""
 print(f"Maximum sugar level: {max(sugar_levels)}")
 print(f"Min sugar level: {min(sugar_levels)}")
 
+base_liquid = ["water", "milk"]
+extra_flavour = ["ginger"]
+full_liquid_mix = base_liquid + extra_flavour # It is called operator overloading works kind of like .extend
+print(f"Mixed liquid: {full_liquid_mix}")
+
+strong_brew = ["black tea", "water"] * 3
+print(f"Print strong brew: {strong_brew}")
+
+raw_spice_data = bytearray(b"CINNAMON") # Advanced Example and topic revise again but only syntax part(Raw Data)
+raw_spice_data = raw_spice_data.replace(b"CINN", b"CARD")
+print(f"Bytes: {raw_spice_data}")
