c557f3447324a33e547d326320112e1889c978c8
Bahauddin Alam <169211765+bahauddin-alam@users.noreply.github.com>
2026-03-19 10:24:19 +0530

    Added file 03_tea_orders.py in folder 04_loops

---
+++ 04_loops/03_tea_orders.py
@@ -0,0 +1,12 @@
+"""
+You receive a list of names for chai orders.
+The goal is to print out the order queue.
+Task:
+Use a list of names.
+Print: "Order ready for [name]
+"""
+Orders = ["Alam", "Hitesh", "Aman", "Carlos"]
+
+
+for name in Orders:
+    print(f"Order ready for {name}")
