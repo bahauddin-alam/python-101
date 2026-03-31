aef61f2fb6485201de193a0eaacb88e4f9048284
Bahauddin Alam <169211765+bahauddin-alam@users.noreply.github.com>
2026-03-31 19:47:45 +0530

    feat: add functions- readability example,lec-39 func-04_read...

---
+++ 05_functions/04_readability.py
@@ -0,0 +1,16 @@
+"""
+You sell diffenrent chai sizes.
+Instead of writing formulas everywhere, create a function.
+Task:
+# Write calculate_bill(cups, price_per_cup)
+# Return total bill
+# Use this function for multiple orders
+"""
+
+def calculate_bill(cups, price_per_cup):
+    return cups * price_per_cup
+
+my_bill = calculate_bill(3, 15)
+print(my_bill)
+
+print("Order for table 2: ", calculate_bill(2, 50))
